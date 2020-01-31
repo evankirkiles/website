8b0438d9de469cfcfa6df2ff2b5550b8a28eac60
Evan Kirkiles <kirkilese@gmail.com>
2020-01-31 12:23:02 -0500

    please work

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from app import create_app, db
 import os
 
 # Build the app with the config specified in config.env
-application = create_app(os.getenv("FLASK_CONFIG") or "default")
+application = create_app(os.getenv("ENV") or "default")
 
 if __name__ == "__main__":
     application.run()
