c83ae73fdb5aa24645367d6ba898a37f63138f34
Evan Kirkiles <kirkilese@gmail.com>
2020-01-31 01:39:05 -0500

    Updated getting env file.

--- config.py
+++ config.py
@@ -9,9 +9,12 @@ env = os.environ.get("ENV")
 # Initialize the base directory for file lookups
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Import environment variables from .config file if necessary
-if os.path.exists(env + ".env"):
+env_file = "/config/production.env" if env == "production" and \
+            os.path.exists("/config/" + env + ".env") else
+            env + ".env" if os.path.exists(env + ".env") else None
+if env_file not None:
     print("Importing env variables from .env file")
-    for line in open(env + ".env"):
+    for line in open(env_file):
         var = line.strip().split("=", 1)
         if len(var) == 2:
             os.environ[var[0]] = var[1].replace('"', "")
