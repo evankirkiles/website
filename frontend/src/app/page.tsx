ad0ddb9751d1fc1d01bff191cc16c91947f31dd1
evan kirkiles <kirkilese@gmail.com>
2023-04-03 11:12:23 -0400

    Added two-column layout.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,19 +10,27 @@ export default function Home() {
   return (
     <main className={s.container}>
       <section className={s.column}>
-        <h1 className={s.title}>Evan Kirkiles</h1>
-        <p>is a student, software engineer, papercrafter, and designer. He currently
-          works as a software engineer at Channel Studio, and will work this summer as an intern
-          at the New York Times.
-        </p>
-        <br />
-        <p>He enjoys:</p>
-        <ul>
-          <li>– Design</li>
-          <li>– Full-stack development</li>
-          <li>– Paper sculpture</li>
-          <li>– Photogrammetry</li>
-        </ul>
+        <div style={{ gridArea: 'a' }}>March 23, 2022</div>
+        <div style={{ gridArea: 'b' }}>1/1</div>
+        <div style={{ gridArea: 's', position: 'relative' }}>
+          <canvas className={s.stars} />
+        </div>
+        <div className={s.main}>
+          <h1 className={s.title}>Evan Kirkiles</h1>
+          <p>
+            is a student in his junior year at Yale University. He
+            currently works as a software engineer at Channel Studio, and will
+            work this summer as an intern at the New York Times.
+          </p>
+          <br />
+          <p>His practices, in no specific order, are:</p>
+          <ul>
+            <li>– Full-stack development</li>
+            <li>– Paper sculpture</li>
+            <li>– Photogrammetry</li>
+            <li>– Design</li>
+          </ul>
+        </div>
       </section>
       <section className={s.column}>hi2</section>
     </main>
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -8,7 +8,6 @@ import s from "@/styles/components/NavBar.module.scss";
 
 export default function NavBar() {
   return <nav className={s.container}>
-    <span>March 10, 2023</span>
     <button className={s.button}>Menu</button>
   </nav>
 }
