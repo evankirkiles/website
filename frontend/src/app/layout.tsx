a56c1d88157a7f31e3a1746b2a792c2b16dd9207
evan <kirkilese@gmail.com>
2023-04-08 15:12:08 -0400

    Update layout.tsx, adding scroll theming back

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,25 +17,23 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
-        {/* <meta id="themecolor" name="theme-color" content="#ffffff" /> */}
+        <meta id="themecolor" name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="title" content="Evan Kirkiles" />
         <meta name="description" content="Programmer, designer, and artist." />
       </head>
-      <body className={AkzidenzGrotesk.variable}>
+      <ScrollThemeColorBody
+        className={AkzidenzGrotesk.variable}
+        initialColor="#ffffff"
+        timeout={300}
+      >
         {children}
         <Nav>
           {/* @ts-expect-error Server Component */}
           <NavContents />
         </Nav>
         <Footer />
-      </body>
-      {/* <ScrollThemeColorBody
-        className={AkzidenzGrotesk.variable}
-        initialColor="#ffffff"
-        timeout={300}
-      >
-      </ScrollThemeColorBody> */}
+      </ScrollThemeColorBody>
     </html>
   );
 }
