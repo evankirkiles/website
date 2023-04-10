92dfd8cf7abdf1d45cb6c4de7210a93b5e11bf18
evan kirkiles <kirkilese@gmail.com>
2023-04-10 02:47:54 -0400

    Fixed theme color bar.

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -10,7 +10,7 @@ export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
 declare const window: Window & { dataLayer: Record<string, unknown>[]; };
 
 export const pageview = (url: string) => {
-  window.dataLayer.push({
+  window.dataLayer?.push({
     event: 'pageview',
     page: url
   });
