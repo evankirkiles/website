0da6d011992b3da49c660c2bad08a5a606b0e1cc
evan <kirkilese@gmail.com>
2023-04-13 09:50:18 -0400

    Fleshed out footer a bit more.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,13 +5,45 @@
  * 2023 evan's personal website,
  */
 import s from './styles.module.scss';
+import { IoLogoGithub, IoLogoInstagram, IoLogoLinkedin } from 'react-icons/io';
 
 export default function Footer() {
   return (
     <div className={s.container}>
-      <span>© 2023 Evan Kirkiles.</span>
-      <span>Typeface: Akzidenz Grotesk.</span>
-      <span>Stack: Next.js 13, Sanity CMS.</span>
+      <div className={s.credits_col}>
+        <p className={s.credits}>© 2023 Evan Kirkiles.</p>
+        <p>Typeface: Akzidenz Grotesk.</p>
+        <p>Stack: Next.js 13, Sanity CMS.</p>
+        <ul className={s.credits_buttons}>
+          <li>
+            <a
+              href="https://www.instagram.com/evankirkiles"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <IoLogoInstagram />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/evankirkiles"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <IoLogoGithub />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://linkedin.com/en/evankirkiles"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <IoLogoLinkedin />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
